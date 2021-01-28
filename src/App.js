bc8d7ea44f97f40264af3f53d5635951e8eed969
project1 <thiettruong1999@gmail.com>
2021-01-28 10:57:28 +0700

    done filter product

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
 import React, { useEffect, useState } from "react";
+import Filter from "./Components/Filter";
 import Products from "./Components/Products";
 import data from "./data.json";
 
 function App() {
-  const [productList, setProductList] = useState();
+  const [productList, setProductList] = useState(data.products);
   const [size, setSize] = useState();
   const [sort, setSort] = useState();
-  useEffect(() => {
-    setProductList(data.products);
-  }, []);
+  const filterProduct = (event) => {
+    if (event.target.value === "") {
+      setProductList(data.products);
+    } else {
+      console.log("hihi", event.target.value);
+      setSize(event.target.value);
+      setProductList(
+        data.products.filter(
+          (product) => product.availableSizes.indexOf(event.target.value) >= 0
+        )
+      );
+    }
+  };
+  const sortProduct = (event) => {
+    const valueSort = event.target.value;
+    setSort(valueSort);
+    setProductList(
+      productList
+        .slice()
+        .sort((a, b) =>
+          valueSort === "lowest"
+            ? a.price < b.price
+              ? 1
+              : -1
+            : valueSort === "highest"
+            ? a.price > b.price
+              ? 1
+              : -1
+            : a._id > b._id
+            ? 1
+            : -1
+        )
+    );
+  };
+
   return (
     <div className="grid-container">
       <header>
@@ -17,7 +50,14 @@ function App() {
       <main>
         <div className="content">
           <div className="main">
-            <Products products={data.products} />
+            <Filter
+              products={productList}
+              size={size}
+              sort={sort}
+              filterProduct={filterProduct}
+              sortProduct={sortProduct}
+            />
+            <Products products={productList} />
           </div>
           <div className="sidebar">Cart Items</div>
         </div>
