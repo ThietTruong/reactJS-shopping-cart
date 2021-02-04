4eba6209986199d78b97e697e7a7c94474122d30
project1 <thiettruong1999@gmail.com>
2021-02-04 12:18:59 +0700

    Done Feature/Animation-modal

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import formatCurrency from "../utill";
+import Fade from "react-reveal/Fade";
 function Cart({ cartItems, removeFromCart, createOrder }) {
   const [showCheckout, setShowCheckout] = useState(false);
   const [inforCheckout, setInforCheckout] = useState();
@@ -31,27 +32,29 @@ function Cart({ cartItems, removeFromCart, createOrder }) {
         )}
         <div>
           <div className="cart">
-            <ul className="cart-items">
-              {cartItems.map((item) => (
-                <li key={item._id}>
-                  <div>
-                    <img alt={item.image} src={item.image}></img>
-                  </div>
-                  <div>
-                    <div>{item.title}</div>
-                    <div className="right">
-                      ${item.price} x {item.count}
-                      <button
-                        className="button"
-                        onClick={() => removeFromCart(item)}
-                      >
-                        Remove from cart
-                      </button>
+            <Fade left cascade>
+              <ul className="cart-items">
+                {cartItems.map((item) => (
+                  <li key={item._id}>
+                    <div>
+                      <img alt={item.image} src={item.image}></img>
                     </div>
-                  </div>
-                </li>
-              ))}
-            </ul>
+                    <div>
+                      <div>{item.title}</div>
+                      <div className="right">
+                        ${item.price} x {item.count}
+                        <button
+                          className="button"
+                          onClick={() => removeFromCart(item)}
+                        >
+                          Remove from cart
+                        </button>
+                      </div>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </Fade>
           </div>
           {cartItems.length === 0 ? (
             ""
@@ -79,44 +82,46 @@ function Cart({ cartItems, removeFromCart, createOrder }) {
               </div>
             </div>
           )}
-          {showCheckout ? (
+          {showCheckout && cartItems.length !== 0 ? (
             <div className="cart">
-              <form onSubmit={handleOrder}>
-                <ul className="form-container">
-                  <li>
-                    <label>Email</label>
-                    <input
-                      name="email"
-                      type="email"
-                      required
-                      onChange={handleInput}
-                    ></input>
-                  </li>
-                  <li>
-                    <label>Name</label>
-                    <input
-                      name="name"
-                      type="text"
-                      required
-                      onChange={handleInput}
-                    ></input>
-                  </li>
-                  <li>
-                    <label>Adress</label>
-                    <input
-                      name="address"
-                      type="text"
-                      required
-                      onChange={handleInput}
-                    ></input>
-                  </li>
-                  <li>
-                    <button className="button primary" type="submit">
-                      Create order
-                    </button>
-                  </li>
-                </ul>
-              </form>
+              <Fade right cascade>
+                <form onSubmit={handleOrder}>
+                  <ul className="form-container">
+                    <li>
+                      <label>Email</label>
+                      <input
+                        name="email"
+                        type="email"
+                        required
+                        onChange={handleInput}
+                      ></input>
+                    </li>
+                    <li>
+                      <label>Name</label>
+                      <input
+                        name="name"
+                        type="text"
+                        required
+                        onChange={handleInput}
+                      ></input>
+                    </li>
+                    <li>
+                      <label>Adress</label>
+                      <input
+                        name="address"
+                        type="text"
+                        required
+                        onChange={handleInput}
+                      ></input>
+                    </li>
+                    <li>
+                      <button className="button primary" type="submit">
+                        Create order
+                      </button>
+                    </li>
+                  </ul>
+                </form>
+              </Fade>
             </div>
           ) : (
             ""
--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import formatCurrency from "../utill";
-
+import Fade from "react-reveal/Fade";
+import Zoom from "react-reveal/Zoom";
+import Modal from "react-modal";
 function Products({ products, addToCart }) {
+  const [showModal, setShowModal] = useState(false);
+  const [modalProduct, setModalProduct] = useState({
+    product: null,
+  });
+  const openModalProduct = (product) => {
+    setModalProduct({
+      product: product,
+    });
+  };
+  const closeModalProduct = () => {
+    setModalProduct({
+      product: null,
+    });
+  };
   return (
     <div>
-      <ul className="products">
-        {products.map((product) => {
-          return (
-            <>
+      <Fade bottom cascade>
+        <ul className="products">
+          {products.map((product) => {
+            return (
               <li key={product._id}>
                 <div className="product">
-                  <a href={`#${product._id}`}>
+                  <a
+                    href={`#${product._id}`}
+                    onClick={() => openModalProduct(product)}
+                  >
                     <img src={product.image} alt={product.image}></img>
                     <p> {product.title}</p>
                   </a>
@@ -26,10 +45,50 @@ function Products({ products, addToCart }) {
                   </div>
                 </div>
               </li>
-            </>
-          );
-        })}
-      </ul>
+            );
+          })}
+        </ul>
+      </Fade>
+      {modalProduct.product && (
+        <Modal isOpen={true} onRequestClose={closeModalProduct}>
+          <Zoom>
+            <button className="close-modal" onClick={closeModalProduct}>
+              x
+            </button>
+            <div className="product-details">
+              <img
+                src={modalProduct.product.image}
+                alt={modalProduct.product.title}
+              ></img>
+              <div className="product-details-description">
+                <p>
+                  <strong>{modalProduct.product.title}</strong>
+                </p>
+                <p>{modalProduct.product.description}</p>
+                <p>
+                  Avaiable Size
+                  {modalProduct.product.availableSizes.map((item) => (
+                    <span>
+                      <button className="button">{item}</button>
+                    </span>
+                  ))}
+                </p>
+                <div className="product-price">
+                  <div>{modalProduct.product.price}</div>
+                  <button
+                    className="button primary"
+                    onClick={() => (
+                      addToCart(modalProduct.product), () => closeModalProduct
+                    )}
+                  >
+                    Add to cart
+                  </button>
+                </div>
+              </div>
+            </div>
+          </Zoom>
+        </Modal>
+      )}
     </div>
   );
 }
